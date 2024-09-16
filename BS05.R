083da2c143a18cebf49c469f69ae22de339160eb
Weerawut Chaiyasomboon <weerawut@live.com>
2024-09-16 10:43:28 +0700

    Done for Chapter 05.

--- BS05.R
+++ BS05.R
@@ -106,12 +106,64 @@ dpois(100,cliams_per_day) ## Ans = 0.03417454
 ppois(100,cliams_per_day) ## Ans = 0.7331074
 
 
+### Exercise 5.6 - 74
+S = 8 # Number of Success in Population
+N = 40 # Number of Population
+n = 3 # Number of randomly selected
+
+## a. Find the probability that none of these students has landed an internship.
+## Find P(X=0)
+x = 0
+dhyper(x,S,N-S,n) ## Ans = 0.5020243
+
+## b. Find the probability that at least one of these students has landed an internship.
+## Find P(X>=1) = 1 - P(X=0)
+1 - dhyper(x,S,N-S,n) ## Ans = 0.4979757
 
 
+### Additional Exercise - 82
+investment_table = tibble(
+  Condition = c("Successful","Some Successful","Failure"),
+  Prob = c(0.1,0.4,0.5),
+  Return = c(5,2,-4)
+)
+
+investment_table = investment_table |> 
+  mutate(
+    x_Px = Return*Prob
+  )
+
+sum(investment_table$x_Px) ### Ans is Expected to loss -0.7 million dollars
+
+### Additional Exercise - 92
+## This is Binomial Distribution problem
+p = 0.19
+n = 30
+## a. What is the probability that exactly ten of the employers have eliminated or plan to eliminate perks? -> Find P(X=10)
+dbinom(10,n,p) ## Ans = 0.02722757
+
+## b. What is the probability that at least ten employers, but no more than 20 employers, have eliminated or plan to eliminate perks? -> P(X<=20) - P(X<=9)
+pbinom(20,n,p) - pbinom(9,n,p) ## Ans = 0.04507909
 
 
+## c. What is the probability that at most eight employers have eliminated or plan to eliminate perks? -> Find P(X<=8)
+pbinom(8,n,p) ## Ans = 0.8996469
 
 
+### Additional Exercise - 98
+## This is Hypergeometric Distribution
+## Let's success case is Republican
+S = 25
+N = 50
+n = 10
+
+## a. What is the probability that there will be five Republicans and five Democrats?
+## Find P(X=5)
+dhyper(5,S,N-S,n) ## Ans = 0.2223625
+
+## b. What is the probability that a majority of the committee will be Republicans?
+## Find P(X>5) = P(X<=10) - P(X<=5)
+phyper(10,S,N-S,n) - phyper(5,S,N-S,n) ## Ans =  0.688265
 
 
 
